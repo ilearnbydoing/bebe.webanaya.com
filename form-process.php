c9fb66626b20e1a1582d411d350b39d8a4aa3d27
ilearnbydoing <dgweb123@gmail.com>
2019-03-01 18:57:09 +0530

    update

--- form-process.php
+++ form-process.php
@@ -1,16 +1,22 @@
 <?php
-$name = $_POST["name"];
+$fname = $_POST["firstname"];
+$lname = $_POST["lastname"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
 $company = $_POST["company"];
+$city = $_POST["city"];
 
-$EmailTo = "masterchef4604@gmail.com";
+$EmailTo = "durgesh@webanaya.com,info@hnhtwenty.com";
 $EmailFrom = "info@hnhtwenty.com";
 $Subject = "Chef Master - You have received New Enquiry";
 
 // prepare email body text
-$Body .= "Name: ";
-$Body .= $name;
+$Body .= "First Name: ";
+$Body .= $fname;
+$Body .= "\n";
+
+$Body .= "Last Name: ";
+$Body .= $lname;
 $Body .= "\n";
 
 $Body .= "Email: ";
@@ -23,6 +29,10 @@ $Body .= "\n";
 
 $Body .= "Company: ";
 $Body .= $company;
+$Body .= "\n";
+
+$Body .= "City: ";
+$Body .= $city;
 $Body .= "\n";
 
 // send email
